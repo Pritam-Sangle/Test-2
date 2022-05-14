58e6236f756e6b41370f182dc00626c5e4c0f941
unknown <sanglepm@gmail.com>
2022-05-15 00:03:53 +0530

    Added hungry prompt

--- py/Hello-World.py
+++ py/Hello-World.py
@@ -7,6 +7,23 @@
 print("Hello World")
 
 
+# In[13]:
+
+
+name = input("What is your name?")
+print("Your Name is", name+'.')
+
+
+# In[15]:
+
+
+hungry=input("Are you hungry?")
+if hungry == 'yes':
+    print('Eat Pizza')
+else:
+    print('do your homework')
+
+
 # In[ ]:
 
 
